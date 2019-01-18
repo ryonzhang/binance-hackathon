f248aa0dd79ece257c196852db77b6152bda9b59
Ruiyang Zhang <zhangruiyang36@gmail.com>
2019-01-18 11:24:46 +0800

    init

---
+++ src/app/pages/tables/smart-table/smart-table.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
+
+import { SmartTableService } from '../../../@core/data/smart-table.service';
+
+@Component({
+  selector: 'ngx-smart-table',
+  templateUrl: './smart-table.component.html',
+  styleUrls: ['./smart-table.component.scss'],
+})
+export class SmartTableComponent {
+
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      id: {
+        title:'Incoming Transaction ID',
+        type: 'string',
+      },
+      amount: {
+        title: 'Amount',
+        type: 'number',
+      },
+      type: {
+        title: 'Danger Level',
+        type: 'number',
+      },
+    },
+    rowClassFunction : (row) => {
+      if (row.data.type === 1) {
+          return 'unknown';
+        } else if (row.data.type === 2) {
+          return 'clean';
+        }
+        return 'danger';
+      },
+  };
+
+  source: LocalDataSource = new LocalDataSource();
+
+  constructor(private service: SmartTableService) {
+    const data = this.service.getData();
+    this.source.load(data);
+  }
+
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  }
+}
